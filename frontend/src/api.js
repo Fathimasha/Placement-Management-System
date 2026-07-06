b5832fc73c00521361ff757d46a990e788a0a8f6
Shaik Fathima <23A81A05B8@sves.org.in>
2026-07-07 00:07:21 +0530

    Fixed frontend API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://placement-management-system-1mbj.onrender.com/api/students',
+  baseURL: 'https://placement-management-system-1mbj.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 });
 
